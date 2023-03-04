2489e2dcf9b8aac412c7d03a4936de8030f06200
Linas Einikis <linas.einikis@mif.stud.vu.lt>
2023-03-04 20:59:32 +0100

    Fixed missing coord error

--- database/collectiontransformers/TranscribusTransformer.py
+++ database/collectiontransformers/TranscribusTransformer.py
@@ -36,16 +36,17 @@ class TranscribusTransformer(CollectionTransformer):
                 transcription = text_line.find('d:TextEquiv/d:Unicode',ns).text
                 points_string = text_line.find('d:Coords', ns).attrib['points']
                 coords = points_string.split(' ')
-                coords = [tuple(map(int, coord.split(','))) for coord in coords]
-                points = np.array(coords)
-                img = orig_image.copy()
-                cropped_image = img[np.min(points[:,1]):np.max(points[:,1]), np.min(points[:,0]):np.max(points[:,0])]
-                if np.any(np.array(cropped_image.shape) == 0) or is_low_contrast(cropped_image):
-                    continue
-                line_image_filename = "{}_line_{}.jpg".format(file.replace('.jpg', ''), index)
-                loc = os.path.join(lines_dir_path, line_image_filename)
-                imsave(loc, cropped_image, check_contrast=False)
-                tr_output += "{}\t{}\n".format(line_image_filename, transcription)
+                if len(coords) > 0:
+                    coords = [tuple(map(int, coord.split(','))) for coord in coords]
+                    points = np.array(coords)
+                    img = orig_image.copy()
+                    cropped_image = img[np.min(points[:,1]):np.max(points[:,1]), np.min(points[:,0]):np.max(points[:,0])]
+                    if np.any(np.array(cropped_image.shape) == 0) or is_low_contrast(cropped_image):
+                        continue
+                    line_image_filename = "{}_line_{}.jpg".format(file.replace('.jpg', ''), index)
+                    loc = os.path.join(lines_dir_path, line_image_filename)
+                    imsave(loc, cropped_image, check_contrast=False)
+                    tr_output += "{}\t{}\n".format(line_image_filename, transcription)
         return tr_output
 
     def process_line_images_polygons(self, file, tr_output, lines_dir_path, root):
